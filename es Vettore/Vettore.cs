22f58ff57b4fc19cb4014599d5c45cd7d779671e
Ele <Ele@DESKTOP-SFOT805>
2024-03-05 22:28:17 +0100

    Aggiungere i file di progetto.

---
+++ es Vettore/Vettore.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_Vettore
+{
+    internal class Vettore
+    {
+        readonly double x;
+        readonly double y;
+        public Vettore(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        
+        public double Angolo
+        {
+            get
+            {
+                return Math.Atan2(y, x);
+            }
+        }
+        public static Vettore operator +(Vettore v1, Vettore v2)
+        {
+            return new Vettore(v1.x + v2.x, v1.y + v2.y);
+        }
+        public static double operator *(Vettore v1, Vettore v2)
+        {
+            return (Math.Sqrt(v1.x * v1.x + v1.y * v1.y)) * (Math.Sqrt(v2.x * v2.x + v2.y * v2.y) * Math.Cos(Angolo.Vettore(v1, v2)));
+        }
+        public static Vettore operator *(Vettore v1, double scalare)
+        {
+            return new Vettore(v1.x * scalare, v1.y * scalare);
+        }
+        public static Vettore operator -(Vettore v1, Vettore v2)
+        {
+            return new Vettore(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}; {1})", x, y);
+        }
+    }
+    static double Angolo(Vettore v1, Vettore v2)
+    {
+        return v1.Angolo - v2.Angolo;
+    }
+    static void Main(string[] args)
+    {
+        Vettore v1 = new Vettore(2, 5);
+        Vettore v2 = new Vettore(3, 4);
+        Vettore v3 = v1 + v2;
+        v3 = v1 - v2;
+        Console.WriteLine(v3.ToString());
+        Console.ReadLine();
+    
+}
+}
